3e92267e57f18a0bd0b1854a90e6f9de6d10e190
Loïc Nogues <nogues.loic@gmail.com>
2015-08-11 20:42:09 +0300

    Put picture as a link

--- src/components/picture.jsx
+++ src/components/picture.jsx
@@ -7,12 +7,12 @@ rotation[8] = 'minus-ninety';
 var Picture = React.createClass({
   render: function() {
     return (
-      <div className="picture">
+      <a className="picture">
         <img
           src={this.props.photo.thumb} 
           width="250px" 
           className={rotation[this.props.photo.orientation]} />
-      </div>
+      </a>
     );
   }
 });
