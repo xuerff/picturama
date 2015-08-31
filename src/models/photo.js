a24dabe77812d196c05a0af9d0b829efc93e3478
Loïc Nogues <nogues.loic@gmail.com>
2015-08-31 21:28:43 +0300

    CSS tweaks + package.json shortcuts

--- src/models/photo.js
+++ src/models/photo.js
@@ -7,13 +7,6 @@ var knex = require('knex')({
   client: 'sqlite3',
   connection: {
     filename: dbFile
-  //},
-  //initialize: () => {
-  //  console.log('INIT');
-  //  this.on('saving', (model) => {
-  //    console.log('SAVING');
-  //    model.set('id', shortid.generate());
-  //  });
   }
 });
 
@@ -23,19 +16,17 @@ var Photo = bookshelf.Model.extend({
   tableName: 'photos',
 
   initialize: function() {
-    console.log('INIT', this);
-
     this.on('saving', (model) => {
-      console.log('SAVING');
       model.set('id', shortid.generate());
     });
   }
 
 }, {
-  getDates: () => {
+  getDates: function() {
     return this.query().distinct('date').orderBy('date', 'desc');
   },
-  getByDate: (date) => {
+
+  getByDate: function(date) {
     return this.query().where({ date: date });
   }
 });
