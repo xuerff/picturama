b9fe5baaf5e741b863f584c08ee4a0a6a819348e
Loïc Nogues <nogues.loic@gmail.com>
2015-10-27 19:33:56 +0300

    Node usb

--- src/browser.js
+++ src/browser.js
@@ -34,29 +34,15 @@ app.on('ready', () => {
   //library.scan();
   library.watch();
 
-  // Detect add/insert 
-  usbDetect.on('add', function(device) { 
-    console.log('add', device); 
-    let vid = device.vendorId;
-    usbDetect.find(vid, function(err, devices) { console.log('find', devices, err); });
+  usb.on('attach', function(device) {
+    //let vid = device.deviceDescriptor.idVendor;
+    //let pid = device.deviceDescriptor.idProduct;
+    //let term = usb.findByIds(vid, pid);
+    console.log('device', device);
+    device.getStringDescriptor(device.deviceDescriptor.iSerialNumber, function(err, data) {
+      console.log('data', data);
+    })
   });
-  usbDetect.on('add:vid', function(device) { console.log('add vid', device); });
-  usbDetect.on('add:vid:pid', function(device) { console.log('add vid pid', device); });
-   
-  // Detect remove 
-  usbDetect.on('remove', function(device) { console.log('remove', device); });
-  usbDetect.on('remove:vid', function(device) { console.log('remove', device); });
-  usbDetect.on('remove:vid:pid', function(device) { console.log('remove', device); });
-   
-  // Detect add or remove (change) 
-  usbDetect.on('change', function(device) { console.log('change', device); });
-  usbDetect.on('change:vid', function(device) { console.log('change', device); });
-  usbDetect.on('change:vid:pid', function(device) { console.log('change', device); });
-   
-  // Get a list of USB devices on your system, optionally filtered by `vid` or `pid` 
-  //usbDetect.find(function(err, devices) { console.log('find', devices, err); });
-  //usbDetect.find(vid, function(err, devices) { console.log('find', devices, err); });
-  //usbDetect.find(vid, pid, function(err, devices) { console.log('find', devices, err); });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
