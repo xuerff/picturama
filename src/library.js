439f25baaed1c470388f0787c15714c384f8ebc9
Loïc Nogues <nogues.loic@gmail.com>
2016-01-29 23:52:46 +0100

    Custom walk lib

--- src/library.js
+++ src/library.js
@@ -1,4 +1,4 @@
-import {ExifImage} from 'exif';
+//import {ExifImage} from 'exif';
 import moment from 'moment';
 import watchr from 'watchr';
 import sharp from 'sharp';
@@ -8,7 +8,9 @@ import Promise from 'bluebird';
 import libraw from 'node-libraw';
 
 import config from './config';
-import walk from './lib/walk';
+
+import exifParser from './lib/exif-parser';
+import walker from './lib/walker';
 
 import Photo from './models/photo';
 import Version from './models/version';
@@ -32,7 +34,6 @@ class Library {
       fs.mkdirSync(config.tmp);
   }
 
-  //walk(root, fileStat, next) {
   walk(fileStat) {
     let allowed = new RegExp(config.acceptedRawFormats.join('$|') + '$', 'i');
     let extract = new RegExp('([^\/]+)\.(' + config.acceptedRawFormats.join('|') + ')$', 'i');
@@ -45,7 +46,6 @@ class Library {
       if (!fs.existsSync(config.thumbs250Path))
         fs.mkdirSync(config.thumbs250Path);
 
-      //console.log('walk', fileStat.name, config.thumbsPath);
       let filename = fileStat.match(extract)[1];
       console.log('each', fileStat, filename);
 
@@ -72,53 +72,40 @@ class Library {
           return new Photo({ title: filename }).fetch();
         })
         .then((photo) => {
-          return new Promise(function (resolve, reject) {
-            new ExifImage({ image: `${config.thumbsPath}/${filename}.thumb.jpg` }, (err, exifData) => {
+          return [ 
+            photo, 
+            exifParser(`${config.thumbsPath}/${filename}.thumb.jpg`)
+          ];
+        })
+        .spread((photo, exifData) => {
+          let createdAt = moment(
+            exifData.image.ModifyDate,
+            'YYYY:MM:DD HH:mm:ss'
+          );
 
-              let createdAt = moment(
-                exifData.image.ModifyDate,
-                'YYYY:MM:DD HH:mm:ss'
-              );
+          let orientation = 1;
 
-              let orientation = 1;
+          if (exifData.image.hasOwnProperty('Orientation'))
+            orientation = exifData.image.Orientation;
 
-              if (exifData.image.hasOwnProperty('Orientation'))
-                orientation = exifData.image.Orientation;
-
-              if (photo)
-                //next();
-                resolve();
-              else
-                return Photo.forge({
-                  title: filename,
-                  extension: fileStat.name.match(/\.(.+)$/i)[1],
-                  orientation,
-                  date: createdAt.format('YYYY-MM-DD'),
-                  created_at: createdAt.toDate(),
-                  exposure_time: exifData.exif.ExposureTime,
-                  iso: exifData.exif.ISO,
-                  aperture: exifData.exif.FNumber,
-                  focal_length: exifData.exif.FocalLength,
-                  master: `${root}/${fileStat.name}`,
-                  thumb_250: `${config.thumbs250Path}/${filename}.jpg`,
-                  thumb: `${config.thumbsPath}/${filename}.thumb.jpg`
-                })
-                .save()
-                .then(() => {
-                  console.log('saved');
-                  //next();
-                  resolve();
-                })
-                .catch((err) => {
-                  console.log('err on save', err);
-                  reject();
-                });
-            });
-          });
-        //})
-        //.catch(function(err) {
-        //  console.log('ERR', err);
-        //  next();
+          if (photo)
+            return;
+          else
+            return Photo.forge({
+              title: filename,
+              extension: fileStat.name.match(/\.(.+)$/i)[1],
+              orientation,
+              date: createdAt.format('YYYY-MM-DD'),
+              created_at: createdAt.toDate(),
+              exposure_time: exifData.exif.ExposureTime,
+              iso: exifData.exif.ISO,
+              aperture: exifData.exif.FNumber,
+              focal_length: exifData.exif.FocalLength,
+              master: `${root}/${fileStat.name}`,
+              thumb_250: `${config.thumbs250Path}/${filename}.jpg`,
+              thumb: `${config.thumbsPath}/${filename}.thumb.jpg`
+            })
+            .save();
         });
 
     } else return false;
@@ -130,7 +117,7 @@ class Library {
     if (!this.path || !this.versionsPath)
       return false;
 
-    walk(this.path)
+    walker(this.path)
       .map(this.walk.bind(this), { 
         concurrency: parseFloat(process.argv[2] || 'Infinity')
       })
@@ -145,27 +132,11 @@ class Library {
           'message': `Finish importing ${pics.length} in ${time.humanize()} `
         });
       });
-    //let walker = Walk.walk(this.path, { followLinks: false });
-
-    //this.mainWindow.webContents.send('start-import', true);
 
     notifier.notify({
       'title': 'Ansel',
       'message': 'Start import'
     });
-
-    //console.log('Start walk', this.path);
-
-    //walker.on('file', this.walk.bind(this));
-
-    //walker.on('end', () => {
-    //  this.mainWindow.webContents.send('finish-import', true);
-
-    //  notifier.notify({
-    //    'title': 'Ansel',
-    //    'message': 'Finish import'
-    //  });
-    //});
   }
 
   watch() {
