54b873e37a70dc28806df3ff387cb3118480683c
Loïc Nogues <nogues.loic@gmail.com>
2016-02-05 13:01:08 +0100

    Change window title in dev mode

--- src/client.js
+++ src/client.js
@@ -5,4 +5,7 @@ import ReactDOM from 'react-dom';
 
 import Ansel from './../dist/components/ansel';
 
+if (process.env.ANSEL_DEV_MODE)
+  document.title = 'Ansel - DEV MODE';
+
 ReactDOM.render(React.createElement(Ansel), document.getElementById('app'));
