62b735a0a017b3fb7f672df3b8a049314ecce419
Loïc Nogues <nogues.loic@gmail.com>
2016-03-02 16:34:36 +0100

    Display picture latest version

--- src/reducers/photos.js
+++ src/reducers/photos.js
@@ -3,8 +3,21 @@ const initialState = [{}];
 export default function photos(state = initialState, action) {
   switch (action.type) {
   case 'GET_PHOTOS_SUCCESS':
-    return action.photos;
+    return action.photos.map(function(photo) {
+      photo.versionNumber = 1;
 
+      if (photo.hasOwnProperty('versions') && photo.versions.length > 0) {
+        photo.versionNumber = 1 + photo.versions.length;
+
+        let lastVersion = photo.versions[photo.versions.length - 1];
+
+        photo.thumb = lastVersion.output;
+        photo.thumb_250 = lastVersion.thumbnail;
+      }
+
+      return photo;
+    });
+ 
   default:
     return state;
   }
