5b1704d62c3d02416e4479a503796749d462393f
Loïc Nogues <nogues.loic@gmail.com>
2016-03-10 17:35:08 +0100

    Create a store file

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 
-//import DevTools from './dev-tools';
 import Ansel from './ansel';
 
-import reducers from './../reducers';
-
-const enhancer = compose(
-  // Middleware you want to use in development:
-  applyMiddleware(thunk)
-  // Required! Enable Redux DevTools with the monitors you chose
-  //DevTools.instrument()
-);
-
-//const store = createStore(reducers, applyMiddleware(thunk));
-const store = createStore(reducers, enhancer);
+import store from './../store';
 
 export default class App extends React.Component {
   render() {
