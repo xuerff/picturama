11e03d120bb8b77be47a310b600fe7b8d87226f4
Loïc Nogues <nogues.loic@gmail.com>
2016-10-19 18:44:20 +0200

    Test the import

--- specs/run.spec.js
+++ specs/run.spec.js
@@ -8,6 +8,26 @@ const rimraf = require('rimraf');
 
 const testsPath = '/tmp/ansel-tests';
 
+var knex = require('knex')({
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: `${testsPath}/dot-ansel/db.sqlite3`
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: `${__dirname}/../migrations`
+  }
+});
+
+const settings = {
+  directories: {
+    photos: `${__dirname}/photos`,
+    versions: `${testsPath}/versions`
+  }
+};
+
+
 describe('application launch', function () {
   this.timeout(200000);
 
@@ -15,13 +35,41 @@ describe('application launch', function () {
     if (!fs.existsSync(testsPath))
       fs.mkdirSync(testsPath);
 
+    if (!fs.existsSync(`${testsPath}/dot-ansel`))
+      fs.mkdirSync(`${testsPath}/dot-ansel`);
+
+    if (!fs.existsSync(`${testsPath}/versions`))
+      fs.mkdirSync(`${testsPath}/versions`);
+
+    fs.writeFileSync(`${testsPath}/dot-ansel/settings.json`, JSON.stringify(settings));
+
     this.app = new Application({
       path: `${__dirname}/../node_modules/.bin/electron`,
       args: [`${__dirname}/../.`],
       env: { ANSEL_TEST_MODE: 1 }
     });
 
+    // Since we're populating manually the settings.json
+    // we need to run the migration manually
+
+    knex.migrate.latest().finally(() => {
+      console.log('migration complete');
+      return knex.destroy(); //works
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+
     return this.app.start()
+      .then(() => {
+        //console.log(this.app.process);
+        return this.app.client.getMainProcessLogs()
+      })
+      .then(function (logs) {
+        logs.forEach(function (log) {
+          console.log(log)
+        })
+      })
       .catch((err) => {
         console.log('err', err);
       });
@@ -45,11 +93,19 @@ describe('application launch', function () {
     });
   });
 
-  it('should fill out the photo folder', function() {
-    return this.app.client.click('#photos-dir').then(function() {
-      console.log('hi');
-    });
+  it('should start the import', function() {
+    return this.app.electron.ipcRenderer.send('start-scanning');
   });
+
+  it('should wait for the import to finish', function() {
+    return this.app.client.waitForExist('#library', 10000);
+  });
+  //it('should fill out the photo folder', function() {
+  //  return this.app.client.setValue("#photos-dir", `${__dirname}/photos`);
+  //  //return this.app.client.click('#photos-dir').then(function() {
+  //  //  console.log('hi');
+  //  //});
+  //});
 
   after(function (done) {
     if (this.app && this.app.isRunning()) {
