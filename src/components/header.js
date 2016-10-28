43bb6f916e332c33b255f91c7b929cff94e2ad96
Loïc Nogues <nogues.loic@gmail.com>
2016-10-28 18:03:56 +0200

    Hamburger menu triggering the sidebar

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,19 @@ class Header extends React.Component {
     className: React.PropTypes.string.isRequired
   }
 
+  //constructor(props) {
+  //  super(props);
+
+  //  this
+  //}
+  showSidebar() {
+    window.dispatchEvent(new Event('core:toggleSidebar'));
+  }
+
   render() {
     return (
       <header className={this.props.className} id="top-bar">
-        <button>
+        <button onClick={this.showSidebar.bind(this)}>
           <i className="fa fa-bars" aria-hidden="true"></i>
         </button>
       </header>
