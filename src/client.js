14e20605ba0ac0b35621203502ceed931a69f3f7
Loïc Nogues <nogues.loic@gmail.com>
2017-02-24 18:42:40 +0100

    Ansel v0.4.0

--- src/client.js
+++ src/client.js
@@ -9,7 +9,7 @@ import pkgs from './../package.json';
 if (process.env.ANSEL_DEV_MODE)
   document.title = 'Ansel - DEV MODE';
 else
-  document.title = `Ansel - ${pkgs.buildnum}`;
+  document.title = `Ansel - ${pkgs.version}`;
 
 if (process.env.ANSEL_TEST_MODE)
   document.title = 'Ansel - TEST MODE';
