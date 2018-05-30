39da35ad85c68b71e34f2058b1e456f0dae73222
Til Schneider <github@murfman.de>
2018-05-30 06:49:24 +0200

    Showing flags in grid

--- src/components/library/Picture.tsx
+++ src/components/library/Picture.tsx
@@ -7,6 +7,7 @@ import { findDOMNode } from 'react-dom'
 import AppState from '../../reducers/AppState'
 import { PhotoType } from '../../models/photo'
 import { bindMany } from '../../util/LangUtil'
+import FaIcon from '../widget/icon/FaIcon'
 
 const { Menu, MenuItem } = remote;
 
@@ -36,7 +37,7 @@ class Picture extends React.Component<Props, State> {
 
     this.state = { showContextMenu: false, thumbnailVersion: Date.now() }
 
-    bindMany(this, 'contextMenu', 'handleClick', 'handleDblClick', 'onThumnailChange')
+    bindMany(this, 'contextMenu', 'onAfterRender', 'positionFlag', 'handleClick', 'handleDblClick', 'onThumnailChange')
   }
 
   contextMenu(e: React.MouseEvent<HTMLImageElement>) {
@@ -92,6 +93,30 @@ class Picture extends React.Component<Props, State> {
     window.removeEventListener('edit:thumnailChange', this.onThumnailChange)
   }
 
+  onAfterRender() {
+    if (this.refs.flag) {
+      const imageElem = findDOMNode(this.refs.image) as HTMLImageElement & { _loadAdded: boolean }
+      if (imageElem.complete) {
+        this.positionFlag()
+      } else if (!imageElem._loadAdded) {
+        imageElem.addEventListener('load', this.positionFlag)
+        imageElem._loadAdded = true
+      }
+    }
+  }
+
+  positionFlag() {
+    if (this.refs.flag) {
+      const pictureElem = findDOMNode(this.refs.picture) as HTMLElement
+      const imageElem = findDOMNode(this.refs.image) as HTMLImageElement
+      const flagElem = findDOMNode(this.refs.flag) as HTMLElement
+
+      flagElem.style.display = 'block'
+      flagElem.style.right  = `${Math.round((pictureElem.offsetWidth  - imageElem.offsetWidth ) / 2)}px`
+      flagElem.style.bottom = `${Math.round((pictureElem.offsetHeight - imageElem.offsetHeight) / 2)}px`
+    }
+  }
+
   onThumnailChange(evt: CustomEvent) {
     const photoId = evt.detail.photoId
     if (photoId === this.props.photo.id) {
@@ -111,6 +136,7 @@ class Picture extends React.Component<Props, State> {
 
   render() {
     let photo = this.props.photo;
+    setTimeout(this.onAfterRender)
     return (
       <a
         ref="picture"
@@ -118,10 +144,14 @@ class Picture extends React.Component<Props, State> {
         onDoubleClick={this.handleDblClick}>
         <span className="v-align"></span>
         <img
+          ref="image"
           onClick={this.handleClick}
           onContextMenu={this.contextMenu}
           src={photo.thumb_250 + '?v=' + this.state.thumbnailVersion}
           className="shadow--2dp" />
+        {!!photo.flag &&
+          <FaIcon ref="flag" className="Picture-flag" name="flag"/>
+        }
       </a>
     );
   }
