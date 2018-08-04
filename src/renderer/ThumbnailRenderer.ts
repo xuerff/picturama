0e20c6c25b2a7da646cc5e9cef8c4f929866812a
Til Schneider <github@murfman.de>
2018-08-04 12:49:22 +0200

    Scan performance: Reduced bottleneck of thumbnail renderer

--- src/renderer/ThumbnailRenderer.ts
+++ src/renderer/ThumbnailRenderer.ts
@@ -2,6 +2,7 @@ import { ExifOrientation } from '../models/DataTypes'
 import { PhotoType, PhotoWork } from '../models/photo'
 import { assertRendererProcess } from '../util/ElectronUtil'
 import PhotoCanvas from './PhotoCanvas'
+import { Texture } from './WebGLCanvas'
 import SerialJobQueue from '../util/SerialJobQueue'
 import Profiler from '../util/Profiler'
 
@@ -9,7 +10,7 @@ import Profiler from '../util/Profiler'
 assertRendererProcess()
 
 
-type RenderJob = { nonRawImgPath: string, orientation: ExifOrientation, photoWork: PhotoWork, profiler: Profiler | null }
+type RenderJob = { nonRawImgPath: string, texture: Texture, orientation: ExifOrientation, photoWork: PhotoWork, profiler: Profiler | null }
 
 const queue = new SerialJobQueue(
     (newJob, existingJob) => (newJob.nonRawImgPath === existingJob.nonRawImgPath) ? newJob : null,
@@ -26,23 +27,25 @@ export async function renderThumbnailForPhoto(photo: PhotoType, photoWork: Photo
 
 
 export async function renderThumbnail(nonRawImgPath: string, orientation: ExifOrientation, photoWork: PhotoWork, profiler: Profiler | null): Promise<string> {
-    return queue.addJob({ nonRawImgPath, orientation, photoWork, profiler })
-}
-
-
-async function renderNextThumbnail(job: RenderJob): Promise<string> {
-    const { nonRawImgPath, orientation, photoWork, profiler } = job
-    if (profiler) profiler.addPoint('Waited in queue')
-
     if (canvas === null) {
         canvas = new PhotoCanvas()
             .setMaxSize(thumbnailSize, thumbnailSize)
         if (profiler) profiler.addPoint('Created canvas')
     }
 
-    await canvas.loadFromSrc(nonRawImgPath, profiler)
+    return canvas.createTextureFromSrc(nonRawImgPath, profiler)
+        .then(texture => {
+            return queue.addJob({ nonRawImgPath, texture, orientation, photoWork, profiler })
+        })
+}
+
+
+async function renderNextThumbnail(job: RenderJob): Promise<string> {
+    const { nonRawImgPath, texture, orientation, photoWork, profiler } = job
+    if (profiler) profiler.addPoint('Waited in queue')
 
     canvas
+        .setBaseTexture(texture)
         .setExifOrientation(orientation)
         .setPhotoWork(photoWork)
         .update()
