71d7e0f823af05f0aabfdfa34b3cc3aa605995c3
Til Schneider <github@murfman.de>
2018-08-14 13:32:28 +0200

    Changed indent

--- src/background/MetaData.ts
+++ src/background/MetaData.ts
@@ -9,59 +9,59 @@ const fileStat = Promise.promisify(fs.stat)
 
 
 export interface MetaData {
-  exposureTime?: number,
-  iso?:          number,
-  aperture?:     number,
-  focalLength?:  number,
-  createdAt:    Date,
-  /** Details on orientation: https://www.impulseadventure.com/photo/exif-orientation.html */
-  orientation:  ExifOrientation,
-  tags:         string[]
+    exposureTime?: number,
+    iso?:          number,
+    aperture?:     number,
+    focalLength?:  number,
+    createdAt:    Date,
+    /** Details on orientation: https://www.impulseadventure.com/photo/exif-orientation.html */
+    orientation:  ExifOrientation,
+    tags:         string[]
 }
 
 
 export function readMetadataOfImage(imagePath: string): Promise<MetaData> {
-  return readExifOfImage(imagePath)
-    .then(extractMetaDataFromExif)
-    .catch(error => {
-      if (error.message !== 'Invalid JPEG section offset') {
-        console.log(`Reading EXIF data from ${imagePath} failed - continuing without. Error: ${error.message}`)
-      }
-      return fileStat(imagePath)
-        .then(stat => ({
-          createdAt: stat.birthtime,
-          orientation: 1,
-          tags: []
-        }))
-    })
+    return readExifOfImage(imagePath)
+        .then(extractMetaDataFromExif)
+        .catch(error => {
+            if (error.message !== 'Invalid JPEG section offset') {
+                console.log(`Reading EXIF data from ${imagePath} failed - continuing without. Error: ${error.message}`)
+            }
+            return fileStat(imagePath)
+                .then(stat => ({
+                    createdAt: stat.birthtime,
+                    orientation: 1,
+                    tags: []
+                }))
+        })
 }
 
 
 function readExifOfImage(imagePath) {
-  return readFile(imagePath)
-    .then(buffer => {
-      const parser = ExifParser.create(buffer) as any
-      return parser.parse()
-    })
+    return readFile(imagePath)
+        .then(buffer => {
+            const parser = ExifParser.create(buffer) as any
+            return parser.parse()
+        })
 }
 
 
 function extractMetaDataFromExif(exifData): MetaData {
-  const exifTags = exifData.tags
-  let metaData = {
-    exposureTime: exifTags.ExposureTime,
-    iso:          exifTags.ISO,
-    aperture:     exifTags.FNumber,
-    focalLength:  exifTags.FocalLength,
-    createdAt:    new Date((exifTags.DateTimeOriginal || exifTags.DateTime || exifTags.CreateDate || exifTags.ModifyDate) * 1000),
-    orientation:  exifTags.Orientation || 1,
-      // Details on orientation: https://www.impulseadventure.com/photo/exif-orientation.html
-    tags:         []
-  };
+    const exifTags = exifData.tags
+    let metaData = {
+        exposureTime: exifTags.ExposureTime,
+        iso:          exifTags.ISO,
+        aperture:     exifTags.FNumber,
+        focalLength:  exifTags.FocalLength,
+        createdAt:    new Date((exifTags.DateTimeOriginal || exifTags.DateTime || exifTags.CreateDate || exifTags.ModifyDate) * 1000),
+        orientation:  exifTags.Orientation || 1,
+            // Details on orientation: https://www.impulseadventure.com/photo/exif-orientation.html
+        tags:         []
+    };
 
-  // TODO: Translate from `exiv2` result into `exif-parser` result
-  //if (exData.hasOwnProperty('Xmp.dc.subject'))
-  //  metaData.tags = exData['Xmp.dc.subject'].split(', ');
+    // TODO: Translate from `exiv2` result into `exif-parser` result
+    //if (exData.hasOwnProperty('Xmp.dc.subject'))
+    //  metaData.tags = exData['Xmp.dc.subject'].split(', ');
 
-  return metaData;
+    return metaData;
 };
