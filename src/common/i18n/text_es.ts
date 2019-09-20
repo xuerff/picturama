edb8793ab9f2a6ba6dd42b86e3151f44b87e1521
ChesuCR <11211829+chesucr@users.noreply.github.com>
2019-09-20 22:59:48 +0200

    [i18n] Translation to Spanish

---
+++ src/common/i18n/text_es.ts
@@ -0,0 +1,81 @@
+export default {
+
+    common_cancel: 'Cancelar',
+    common_backToLibrary: 'Volver a la biblioteca',
+
+    ErrorController_copied: 'Informe copiado al portapapeles.',
+
+    ErrorToast_title: 'Oh no! Algo ha ido mal.',
+    ErrorToast_hideReport: 'Ocultar informe',
+    ErrorToast_showReport: 'Mostrar informe',
+    ErrorToast_copy: 'Copiar',
+
+    ImportProgressButton_phase_scan_dirs: 'Escaneando carpetas',
+    ImportProgressButton_phase_cleanup: 'Limpiando',
+    ImportProgressButton_phase_import_photos: 'Importando',
+    ImportProgressButton_phase_error: 'Escaneo fallido',
+    ImportProgressButton_ratio: '{0} of {1}',
+    ImportProgressButton_resume: 'Reanudar',
+    ImportProgressButton_pause: 'Pausa',
+
+    ImportController_importFinished: 'Escaneadas {0} fotos en {1}',
+
+    Library_noPhotos_title: 'No hay fotos añadidas',
+    Library_noSettings_message: 'Haz click en el {0} arriba para abrir los ajustes.',
+    Library_noPhotos_message: 'Presiona {0} o el botón más abajo para empezar el escaneo de las fotos en las carpetas.',
+    Library_startScanning: 'Comenzar el escaneo',
+    Library_emptyTrash: 'La papelera está vacía',
+    Library_emptyFavorites: "No mascaste ninguna foto como favorita todavía",
+    Library_emptyView: "No hay ninguna foto en tu vista actual",
+    Library_selectOtherView: 'Por favor, selecciona otra vista en la parte superior a la izquierda.',
+
+    LibraryBottomBar_selected: '{0} selectionados',
+    LibraryBottomBar_deselect: 'Deseleccionar',
+    LibraryBottomBar_photoCount_one: '1 foto',
+    LibraryBottomBar_photoCount_more: '{0} photos',
+
+    LibraryFilterButton_shown: 'Mostrado: {0}',
+    LibraryFilterButton_filter_all: 'Todoas las fotos',
+    LibraryFilterButton_filter_flagged: 'Favoritos',
+    LibraryFilterButton_filter_processed: 'Editado',
+    LibraryFilterButton_filter_trash: 'Papelera',
+    LibraryFilterButton_filter_tag: 'Con etiqueta "{0}"',
+    LibraryFilterButton_tag_title: 'Con etiqueta:',
+
+    LibraryTopBar_emptyTrashQuestion: 'Estás seguro de que quieres vaciar la papelera?',
+    LibraryTopBar_moveToTrash: 'Mover a la papelera del sistema operativo',
+    LibraryTopBar_emptyTrash: 'Vaciar papelera',
+
+    PhotoActionButtons_movedToTrash_one: 'Mover foto a la papelera',
+    PhotoActionButtons_movedToTrash_more: 'Movidas {0} fotos a la papelera',
+    PhotoActionButtons_restoredFromTrash_one: 'Foto restaurada de la papelera',
+    PhotoActionButtons_restoredFromTrash_more: 'Restauradas {0} fotos de la papelera',
+    PhotoActionButtons_rotateLeft: 'Rotar a la izquierda',
+    PhotoActionButtons_rotateRight: 'Rotar a la derecha',
+    PhotoActionButtons_removeFavorite: 'Elminar de favoritos',
+    PhotoActionButtons_addFavorite: 'Añadir a favoritos',
+    PhotoActionButtons_trash: 'Mover a la papelera',
+    PhotoActionButtons_restoreFromTrash: 'Restaurar de la papelera',
+    PhotoActionButtons_restore: 'Restaurar',
+    PhotoActionButtons_photoInfo: 'Mostrar información sobre la foto seleccionada',
+    PhotoActionButtons_export: 'Exportar',
+
+    PhotoInfo_title: 'Info',
+    PhotoInfo_showInFolder: 'Mostrar foto en carpeta',
+    PhotoInfo_copyPath: 'Copiar ruta',
+    PhotoInfo_noSelection_title: 'No hay fotos seleccionadas',
+    PhotoInfo_noSelection_message: 'Por favor, selecciona una foto de la izquierda.',
+
+    PictureDetail_prevPhoto: 'Anterior foto',
+    PictureDetail_nextPhoto: 'Siguiente foto',
+
+    Settings_title: 'Ajustes',
+    Settings_selectPhotoDirs: 'Por favor, selecciona carpetas para escanear imágenes.',
+    Settings_photoDirs: 'Carpetas para escanear imágenes:',
+    Settings_addPhotoDir: 'Añadir carpeta de fotos',
+    Settings_startScan: 'Scanear en busca de imágenes',
+
+    TagEditor_addTag: 'Añadir etiqueta...',
+    TagEditor_error_tagExists: 'Por favor, introduce una nueva etiqueta',
+
+}
