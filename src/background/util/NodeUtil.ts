71fce5bdaf7dce5785a19bad4e32887fa6c3ba8f
Til Schneider <github@murfman.de>
2019-10-18 10:00:20 +0200

    Fix deprecation warning

--- src/background/util/NodeUtil.ts
+++ src/background/util/NodeUtil.ts
@@ -2,5 +2,5 @@ export function parseImageDataUrl(dataUrl: string): Buffer {
     // Example data URL: 'data:image/webp;base64,UklG...'
     const dataPrefix = 'base64,'
     const base64Data = dataUrl.substr(dataUrl.indexOf(dataPrefix) + dataPrefix.length)
-    return new Buffer(base64Data, 'base64')
+    return Buffer.from(base64Data, 'base64')
 }
