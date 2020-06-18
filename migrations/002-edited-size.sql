98acac4b9b268e5f0915f5d51f6230d1666b5d3b
Til Schneider <github@murfman.de>
2020-06-18 12:22:56 +0200

    Fix down migration

--- migrations/002-edited-size.sql
+++ migrations/002-edited-size.sql
@@ -6,7 +6,8 @@
 PRAGMA foreign_keys=OFF;
 
 DROP INDEX photos_master_dir_index;
-CREATE TABLE "photos_new" (
+ALTER TABLE photos RENAME TO photos_old;
+CREATE TABLE "photos" (
     "id"              integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     "master_dir"      text NOT NULL,
     "master_filename" text NOT NULL,
@@ -28,11 +29,10 @@ CREATE TABLE "photos_new" (
     "flag"            boolean NOT NULL DEFAULT '0',
     "trashed"         boolean NOT NULL DEFAULT '0'
 );
-INSERT INTO photos_new (id, master_dir, master_filename, master_width, master_height, master_is_raw, edited_width, edited_height, date_section, created_at, updated_at, imported_at, orientation, camera, exposure_time, iso, focal_length, aperture, flag, trashed)
+INSERT INTO photos (id, master_dir, master_filename, master_width, master_height, master_is_raw, edited_width, edited_height, date_section, created_at, updated_at, imported_at, orientation, camera, exposure_time, iso, focal_length, aperture, flag, trashed)
     SELECT id, master_dir, master_filename, master_width, master_height, master_is_raw, master_width, master_height, date_section, created_at, updated_at, imported_at, orientation, camera, exposure_time, iso, focal_length, aperture, flag, trashed
-    FROM photos;
-DROP TABLE photos;
-ALTER TABLE photos_new RENAME TO photos;
+    FROM photos_old;
+DROP TABLE photos_old;
 CREATE INDEX photos_master_dir_index ON photos(master_dir);
 CREATE INDEX photos_date_section_index ON photos(date_section);
 
@@ -45,6 +45,7 @@ PRAGMA foreign_keys=OFF;
 
 DROP INDEX photos_master_dir_index;
 DROP INDEX photos_date_section_index;
+ALTER TABLE photos RENAME TO photos_old;
 CREATE TABLE "photos" (
     "id"              integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     "master_dir"      text NOT NULL,
@@ -65,11 +66,10 @@ CREATE TABLE "photos" (
     "flag"            boolean NOT NULL DEFAULT '0',
     "trashed"         boolean NOT NULL DEFAULT '0'
 );
-INSERT INTO photos_new (id, master_dir, master_filename, master_width, master_height, master_is_raw, date_section, created_at, updated_at, imported_at, orientation, camera, exposure_time, iso, focal_length, aperture, flag, trashed)
+INSERT INTO photos (id, master_dir, master_filename, master_width, master_height, master_is_raw, date_section, created_at, updated_at, imported_at, orientation, camera, exposure_time, iso, focal_length, aperture, flag, trashed)
     SELECT id, master_dir, master_filename, master_width, master_height, master_is_raw, date_section, created_at, updated_at, imported_at, orientation, camera, exposure_time, iso, focal_length, aperture, flag, trashed
-    FROM photos;
-DROP TABLE photos;
-ALTER TABLE photos_new RENAME TO photos;
+    FROM photos_old;
+DROP TABLE photos_old;
 CREATE INDEX photos_master_dir_index ON photos(master_dir);
 
 PRAGMA foreign_key_check;
